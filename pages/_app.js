fb61cdeec770e16f25747ebeb0491b6954346800
Charlie Rose <charlierose@charlies-air.lan>
2022-10-31 02:09:29 -0400

    light theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,23 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from "@mui/material";
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+function CharlesRose({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+  <ThemeProvider theme={lightTheme}>
+  <Component {...pageProps} />
+  </ThemeProvider>
+  </React.Fragment>
+  )
 }
 
-export default MyApp
+export default CharlesRose
